ecf0a5b6c7679c8896db0a3ea69abef392a831c0
lenny alimwa <lensig13@gmail.com>
2020-08-29 11:01:20 +0300

    ft-add-base-code-for-JVZIPN-API-Controller

---
+++ app/Http/Controllers/API/JVZIPNAPIController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class JVZIPNAPIController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function handler()
+    {
+
+    }
+}
