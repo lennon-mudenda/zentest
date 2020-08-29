11babd03048664e4f467b16a4fd21624db588572
lenny alimwa <lensig13@gmail.com>
2020-08-29 12:11:02 +0300

    ft-add-customers-model

---
+++ database/migrations/2020_08_29_082040_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ccustname', 510);
+            $table->string('ccuststate', 2);
+            $table->string('ccustcc', 2);
+            $table->string('email')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
