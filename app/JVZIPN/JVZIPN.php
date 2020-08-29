c7ce89f5a437caa66b944da288809f1019655ed3
lenny alimwa <lensig13@gmail.com>
2020-08-29 10:49:13 +0300

    ft-add-base-function-for-JVZIPN-request-verifications

---
+++ app/JVZIPN/JVZIPN.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\JVZIPN;
+
+
+class JVZIPN
+{
+    function jvzipnVerification($post) {
+        $secretKey = env("JVZIPN_SECRET_KEY");
+        $pop = "";
+        $ipnFields = array();
+        foreach ($post AS $key => $value) {
+            if ($key == "cverify") {
+                continue;
+            }
+            $ipnFields[] = $key;
+        }
+        sort($ipnFields);
+        foreach ($ipnFields as $field) {
+            // if Magic Quotes are enabled $_POST[$field] will need to be
+            // un-escaped before being appended to $pop
+            $pop = $pop . $post[$field] . "|";
+        }
+        $pop = $pop . $secretKey;
+        if ('UTF-8' != mb_detect_encoding($pop))
+        {
+            $pop = mb_convert_encoding($pop, "UTF-8");
+        }
+        $calcedVerify = sha1($pop);
+        $calcedVerify = strtoupper(substr($calcedVerify,0,8));
+        return $calcedVerify == $post["cverify"];
+    }
+}
