ce2ee5c65c0f0a0bc1b1587be9caa8614fc11ef6
lenny alimwa <lensig13@gmail.com>
2020-08-30 09:23:01 +0300

    ft-add-customers-and-transactions-dashboard-controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +26,45 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Show the customers dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function customers()
+    {
+        return view('customers');
+    }
+
+    /**
+     * Show the customer detail.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function customer(Customer $customer)
+    {
+        return view('customer');
+    }
+
+    /**
+     * Show the transactions dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function transactions()
+    {
+        return view('transactions');
+    }
+
+    /**
+     * Show the transaction detail.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function transaction(Transaction $transaction)
+    {
+        return view('transaction');
     }
 }
